d1232815c452135d057540e5bcabab689e80c43b
Jan Jakubowski <jan.jakubowski.ti@gmail.com>
2023-11-24 01:46:57 +0100

    Added path wildcard support (path tree)

--- server/src/main/java/org/biblioteka/usecase/UseCaseController.java
+++ server/src/main/java/org/biblioteka/usecase/UseCaseController.java
@@ -3,7 +3,6 @@ package org.biblioteka.usecase;
 import org.biblioteka.exceptions.ConfigError;
 import org.biblioteka.http.HttpMethod;
 
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -11,25 +10,73 @@ public class UseCaseController {
 
     private static UseCaseController instance = null;
 
-    private Map<String, Map<HttpMethod, UseCase<?, ?>>> uriMethodUseCaseMap = new HashMap<>(10);
+    private final PathTreeNode root = new PathTreeNode();
 
     public static UseCaseController getInstance() {
-       if(instance == null) {
-           instance = new UseCaseController();
-       }
-       return instance;
+        if (instance == null) {
+            instance = new UseCaseController();
+        }
+        return instance;
     }
 
-    public void registerUseCase(final String uri, final HttpMethod method, final UseCase<?,?> useCase) {
-        uriMethodUseCaseMap.putIfAbsent(uri, new HashMap<>());
-        if(uriMethodUseCaseMap.get(uri).containsKey(method)) {
-            throw new ConfigError("Use case path and method duplicate");
+    public void registerUseCase(final String uri, final HttpMethod method, final UseCase<?, ?> useCase) {
+        PathTreeNode node = root;
+        String[] segments = uri
+                .replaceFirst("^/", "")
+                .split("/", -1);
+
+        for (int i = 0; i < segments.length; i++) {
+            PathTreeNode child = new PathTreeNode();
+            child.segment = segments[i];
+            child = node.addChild(child);
+            node = child;
         }
-        uriMethodUseCaseMap.get(uri).put(method, useCase);
+
+        node.addUseCase(method, useCase);
     }
 
     public UseCase<?, ?> getUseCase(final String uri, final HttpMethod method) {
-        return uriMethodUseCaseMap.getOrDefault(uri, Collections.emptyMap()).get(method);
+        PathTreeNode node = root;
+        String[] segments = uri
+                .replaceFirst("^/", "")
+                .split("/", -1);
+
+        for (int i = 0; i < segments.length; i++) {
+
+            PathTreeNode child = node.children.get(segments[i]);
+
+            if (child == null) {
+                child = node.children.get("*");
+            }
+
+            if (child == null) {
+                return null;
+            }
+
+            node = child;
+        }
+
+        return node.methodUseCaseMap.get(method);
+    }
+
+    private static class PathTreeNode {
+
+        String segment;
+        Map<String, PathTreeNode> children = new HashMap<>(0);
+
+        Map<HttpMethod, UseCase<?, ?>> methodUseCaseMap = new HashMap<>();
+
+        public PathTreeNode addChild(PathTreeNode child) {
+            children.putIfAbsent(child.segment, child);
+            return children.get(child.segment);
+        }
+
+        public void addUseCase(HttpMethod method, UseCase<?, ?> useCase) {
+            if (methodUseCaseMap.containsKey(method)) {
+                throw new ConfigError("Duplicate useCase for method and path");
+            }
+            methodUseCaseMap.put(method, useCase);
+        }
     }
 
 }
